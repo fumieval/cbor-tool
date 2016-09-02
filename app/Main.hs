03c5aa27b6ee13bf049ecc14ca4426bcf080445e
Fumiaki Kinoshita <fumiexcel@gmail.com>
2016-09-02 17:10:07 +0900

    WIP

--- app/Main.hs
+++ app/Main.hs
@@ -1,119 +1,69 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, MultiWayIf #-}
 
 import Data.Binary.Serialise.CBOR
 import Data.Binary.Serialise.CBOR.Term
-import qualified Data.Text as T
 import Data.List
 import System.Environment (getArgs)
-import Text.Trifecta
 import qualified Data.ByteString.Lazy.Char8 as BL
 import Control.Lens hiding (List, Context(..))
 import Control.Monad
-import System.IO (stdin)
-import Data.Either (partitionEithers)
-import Data.List.Lens
+import System.IO
+import System.Console.ANSI
 
-diag :: Term -> String
-diag (TInt i) = show i
-diag (TInteger i) = show i
-diag (TBytes s) = show s
-diag (TBytesI s) = show s
-diag (TString s) = show s
-diag (TStringI s) = show s
-diag (TList xs) = "[" ++ intercalate ", " (map diag xs) ++ "]"
-diag (TListI xs) = "[" ++ intercalate ", " (map diag xs) ++ "]"
-diag (TMap xs) = "{" ++ intercalate ", " (map (\(k,v) -> diag k ++ ": " ++ diag v) xs) ++ "}"
-diag (TMapI xs) = "{" ++ intercalate ", " (map (\(k,v) -> diag k ++ ": " ++ diag v) xs) ++ "}"
-diag (TTagged i x) = show i ++ "(" ++ diag x ++ ")"
-diag (TBool x) = show x
-diag TNull = "()"
-diag TUndef = "undefined"
-diag (TSimple x) = show x
-diag (THalf x) = show x
-diag (TFloat x) = show x
-diag (TDouble x) = show x
+data Doc = DStr String | DBrackets [Doc] | DBraces [Doc] | DDocs [Doc]
 
-data Expr = Traverse
-  | Index Int
-  | Key String
-  | Diag
-  | Row
-  | Indent
-  | Context
-  | Comp Expr Expr
-  | List [Expr] deriving Show
+diagKeyValue :: Term -> Term -> Doc
+diagKeyValue k v = DDocs [DStr $ setSGRCode [SetColor Foreground Vivid Yellow, SetConsoleIntensity BoldIntensity]
+    , diag k
+    , DStr $ setSGRCode [Reset]
+    , DStr ": "
+    , diag v]
 
-data Val = VT Term
-  | VS String
-  | VCxt Val Val
-  | VIndent Val
-  | VList [Val]
-  | VRow [Val]
-  deriving Show
+diag :: Term -> Doc
+diag (TInt i) = DStr $ show i
+diag (TInteger i) = DStr $ show i
+diag (TBytes s) = DStr $ show s
+diag (TBytesI s) = DStr $ show s
+diag (TString s) = DStr $ show s
+diag (TStringI s) = DStr $ show s
+diag (TList xs) = DBrackets (map diag xs)
+diag (TListI xs) = DBrackets (map diag xs)
+diag (TMap xs) = DBraces $ map (uncurry diagKeyValue) xs
+diag (TMapI xs) = DBraces $ map (uncurry diagKeyValue) xs
+diag (TTagged i x) = DDocs [DStr (show i), DStr "(", diag x, DStr ")"]
+diag (TBool x) = DStr $ show x
+diag TNull = DStr "()"
+diag TUndef = DStr "undefined"
+diag (TSimple x) = DStr $ show x
+diag (THalf x) = DStr $ show x
+diag (TFloat x) = DStr $ show x
+diag (TDouble x) = DStr $ show x
 
-runExpr :: [Val] -> Expr -> Val -> [Val]
-runExpr _ Context (VCxt c _) = [c]
-runExpr _ Context _ = []
-runExpr _ Traverse (VList xs) = xs
-runExpr _ Traverse (VRow xs) = xs
-runExpr _ Traverse (VT (TMap kvs)) = [VCxt (VT k) (VT v) | (k, v) <- kvs]
-runExpr _ Traverse (VT (TMapI kvs)) = [VCxt (VT k) (VT v) | (k, v) <- kvs]
-runExpr _ Traverse (VT (TList xs)) = map VT xs
-runExpr _ Traverse (VT (TListI xs)) = map VT xs
-runExpr _ (Key k0) (VT (TMap kvs)) = [VT v | (k, v) <- kvs, matchKey k0 k]
-runExpr _ (Key k0) (VT (TMapI kvs)) = [VT v | (k, v) <- kvs, matchKey k0 k]
-runExpr vs (Key k) (VCxt _ x) = runExpr vs (Key k) x
-runExpr _ Diag v = [VS $ printVal v]
-runExpr vs (Comp a b) t = concatMap (runExpr vs b) $ runExpr vs a t
-runExpr _ Indent v = [VIndent v]
-runExpr vs (List xs) t = [VList $ concatMap (flip (runExpr vs) t) xs]
-runExpr _ Row (VList xs) = [VRow xs]
-runExpr vs e (VCxt _ x) = runExpr vs e x
-runExpr vs e (VIndent x) = VIndent <$> runExpr vs e x
-runExpr _ Traverse v = error $ show v ++ " is not traversable"
-runExpr _ (Key _) v = error $ show v ++ " is not a map"
-runExpr _ t v = error $ show (t, v)
+showDoc :: Int -> Doc -> String
+showDoc _ (DStr s) = s
+showDoc n (DDocs ds) = concatMap (showDoc n) ds
+showDoc _ (DBrackets []) = "[]"
+showDoc n (DBrackets xs)
+  | length (concat ss) < 80 = "[" ++ intercalate ", " ss ++ "]"
+  | otherwise = intercalate "\n" $ _head %~ ("[ "++) $ (ss & _tail . traverse %~ ((replicate n ' ' ++ ", ")++)) ++ [replicate n ' ' ++ "]"]
+  where
+    ss = map (showDoc (n + 2)) xs
+showDoc _ (DBraces []) = "{}"
+showDoc n (DBraces xs)
+  | length (concat ss) < 80 = "{" ++ intercalate ", " ss ++ "}"
+  | otherwise = intercalate "\n" $ _head %~ ("{ "++) $ (ss & _tail . traverse %~ ((replicate n ' ' ++ ", ")++)) ++ [replicate n ' ' ++ "}"]
+  where
+    ss = map (showDoc (n + 2)) xs
 
-_VS f (VS s) = VS <$> f s
-_VS _ x = pure x
-
-matchKey :: String -> Term -> Bool
-matchKey str (TString str') = str == T.unpack str'
-matchKey _ _ = False
-
-parseTerm :: Parser Expr
-parseTerm = choice
-  [ parens parseExpr
-  , List <$> brackets (sepBy parseExpr (symbol ","))
-  , Traverse <$ symbol "*"
-  , Diag <$ symbol "diag"
-  , Indent <$ symbol ">"
-  , Context <$ symbol "?"
-  , Row <$ symbol "row"
-  , Index <$> fromInteger <$> natural
-  , Key <$> stringLiteral
-  ]
-
-parseExpr :: Parser Expr
-parseExpr = foldl Comp <$> parseTerm <*> many parseTerm
-
-printVal :: Val -> String
-printVal (VS x) = x
-printVal (VT t) = diag t
-printVal (VRow xs) = intercalate "\t" $ map printVal xs
-printVal (VList xs) = "[" ++ intercalate ", " (map printVal xs) ++ "]"
-printVal (VIndent v) = "  " ++ printVal v
-printVal (VCxt _ x) = printVal x
-
+main :: IO ()
 main = do
-  (src, flags) <- partitionEithers . map (matching (prefixed "--")) <$> getArgs
-  case parseString (parseExpr <* eof) mempty $ unwords src of
-    Success expr
-      | "multiple" `elem` flags -> forever $ do
-          n <- read <$> getLine
-          term <- deserialise <$> BL.hGet stdin n
-          mapM_ (putStrLn . printVal) $ runExpr [] expr $ VT term
-      | otherwise -> do
-          term <- deserialise <$> BL.getContents
-          mapM_ (putStrLn . printVal) $ runExpr [] expr $ VT term
-    Failure doc -> print doc
+  args <- getArgs
+  hPutStrLn stderr $ show args
+  if
+    | "--multiple" `elem` args -> forever $ do
+      n <- read <$> getLine
+      term <- deserialise <$> BL.hGet stdin n
+      putStrLn $ showDoc 0 $ diag term
+    | otherwise -> do
+      term <- deserialise <$> BL.getContents
+      putStrLn $ showDoc 0 $ diag term
